2ce31571d861eab5d6b38ef526dfb7c90cef2575
Petrus <62590648+petrus-giannini@users.noreply.github.com>
2020-03-24 15:14:40 +0100

    Add files via upload

--- L.Control.Custombox.js
+++ L.Control.Custombox.js
@@ -4,7 +4,11 @@
 L.Control.customBox = L.Control.extend({
     onAdd: function(map) {
         var container = L.DomUtil.create('div', 'leflet-control-monitor');
-        container.innerHTML = "<b>Last update:</b> <span id='lbl_last_update'></span";
+        L.DomEvent.disableClickPropagation(container);
+        container.innerHTML = "<b>Giorno:</b> <span id='lbl_last_update'></span>";
+        container.innerHTML += "<div id='time-slider'><div id='time-slider-handle' class='ui-slider-handle'></div></div>";
+        
+        
         return container;
     },
 
