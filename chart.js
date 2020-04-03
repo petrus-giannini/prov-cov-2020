d932aa56f2b2a8ecbc2e73b54ae7a70a1596e3fb
Petrus <62590648+petrus-giannini@users.noreply.github.com>
2020-04-03 19:47:21 +0200

    Add files via upload

--- chart.js
+++ chart.js
@@ -5,3 +5,69 @@ google.charts.setOnLoadCallback(chartReadyCB);
 function chartReadyCB(){
 	chartReady = true;
 }
+
+function featureinfo(prop){
+	var Provincia = prop.DEN_UTS;
+	var Popolazione = prop.POP;
+	var Value = data[days[days.length-1]][prop.COD_PROV];
+	var values = []; 
+	
+	var Panel = $("<div />")
+		.append(
+			$('<p />').html(
+				"Provincia di <strong>" + Provincia + "</strong><br />Popolazione: " + (Popolazione).toLocaleString("it") + "<br />" + 
+				"Casi al " + displayDate(formatDate(new Date())) + ": " + (Value).toLocaleString("it") + " (" + (Math.round((Value*100/Popolazione)*100)/100).toLocaleString("it") + "%)"
+			)
+		);
+
+	if (chartReady == true){
+		
+		// primo grafico, andamento totale dei casi
+		var arCasi = [['Giorno', 'Casi', { role: 'style' }]];
+    	for(var i=0; i<days.length; i++){
+    		arCasi.push([displayDate(days[i], true), data[days[i]][prop.COD_PROV], colorCasi]);
+    	}
+		var chartdata = google.visualization.arrayToDataTable(arCasi);
+
+        var options = {
+        	'title' : 'Totale dei casi',
+			'width' : chartWitdth,
+			'height' : chartHeight,
+			'legend' : 'none',
+			'backgroundColor' : '#eee'
+		};
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(chartdata, options);
+
+        $("#chart_div").detach().appendTo(Panel);
+
+        // secondo grafico, andamento differenziale 
+		var arCasi2 = [['Giorno', 'trend %', { role: 'style' }]];
+    	for(var i=1; i<days.length; i++){
+    		// aumento: oggi meno ieri
+    		var increase = data[days[i]][prop.COD_PROV] - data[days[i-1]][prop.COD_PROV];
+    		// percentuale dell'aumento sui casi totali
+    		var trend = (increase / data[days[i]][prop.COD_PROV]) * 100;
+    		
+    		arCasi2.push([displayDate(days[i], true), trend, colorTrend]);
+    	}
+		var chartdata2 = google.visualization.arrayToDataTable(arCasi2);
+
+        var options2 = {
+        	'title' : 'Differenza rispetto al giorno precedente, percentuale su numero totale casi (trend)',
+			'width' : chartWitdth,
+			'height' : chartHeight,
+			'legend' : 'none',
+			'backgroundColor' : '#eee',
+			'vAxis' : {format: '#\'%\''}
+		};
+
+        var chart2 = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
+        chart2.draw(chartdata2, options2);
+
+        $("#chart_div2").detach().appendTo(Panel);
+	}
+	
+	return Panel[0];
+}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var data_url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json";
 var theMap;
 var theMarker;
-var geojsonLayer;
+var geojsonLayerCasi;
+var geojsonLayerTrend;
 var marker_coord = [41.957002,12.4844508];
 // array dei giorni
 var days;
@@ -9,75 +9,20 @@ var today;
 // array dei dati
 var data = {};
 // coefficiente di opacità da moltiplicare al valore per ottenere maggiore visibilità della campitura
-var opacityCoefficient = 3;
+var opacityCoefficient = 2.5;
 // handle intervallo
-var cron = null;;
+var cron = null;
 
-function featureinfo(prop){
-	var Provincia = prop.DEN_UTS;
-	var Popolazione = prop.POP;
-	var Value = data[days[days.length-1]][prop.COD_PROV];
-	var values = []; 
-	
-	var Panel = $("<div />")
-		.append(
-			$('<p />').html(
-				"Provincia di <strong>" + Provincia + "</strong><br />Popolazione: " + (Popolazione).toLocaleString("it") + "<br />" + 
-				"Casi al " + displayDate(formatDate(new Date())) + ": " + (Value).toLocaleString("it") + " (" + (Math.round((Value*100/Popolazione)*100)/100).toLocaleString("it") + "%)"
-			)
-		);
+// parametri
+// province
+var data_url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json";
+// grafici
+var chartWitdth = 420;
+var chartHeight = 200;
+var colorCasi = '#FC4E2A';
+var colorTrend = '#FC902A';
+var colorConfini = '#FFAA22';
 
-	if (chartReady == true){
-		
-		// primo grafico, andamento totale dei casi
-		var arCasi = [['Giorno', 'Casi', { role: 'style' }]];
-    	for(var i=0; i<days.length; i++){
-    		arCasi.push([displayDate(days[i], true), data[days[i]][prop.COD_PROV], '#FC4E2A']);
-    	}
-		var chartdata = google.visualization.arrayToDataTable(arCasi);
-
-        var options = {
-        	'title' : 'Totale dei casi',
-			'width' : 420,
-			'height' : 200,
-			'legend' : 'none',
-			'backgroundColor' : '#eee'
-		};
-
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-        chart.draw(chartdata, options);
-
-        $("#chart_div").detach().appendTo(Panel);
-
-        // secondo grafico, andamento differenziale 
-		var arCasi2 = [['Giorno', 'trend %', { role: 'style' }]];
-    	for(var i=1; i<days.length; i++){
-    		// aumento: oggi meno ieri
-    		var increase = data[days[i]][prop.COD_PROV] - data[days[i-1]][prop.COD_PROV];
-    		// percentuale dell'aumento sui casi totali
-    		var trend = (increase / data[days[i]][prop.COD_PROV]) * 100;
-    		
-    		arCasi2.push([displayDate(days[i], true), trend, '#FC902A']);
-    	}
-		var chartdata2 = google.visualization.arrayToDataTable(arCasi2);
-
-        var options2 = {
-        	'title' : 'Differenza rispetto al giorno precedente, percentuale su numero totale casi (trend)',
-			'width' : 420,
-			'height' : 200,
-			'legend' : 'none',
-			'backgroundColor' : '#eee',
-			'vAxis' : {format: '#\'%\''}
-		};
-
-        var chart2 = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
-        chart2.draw(chartdata2, options2);
-
-        $("#chart_div2").detach().appendTo(Panel);
-	}
-	
-	return Panel[0];
-}
 
 function formatDate(date) {
     var d = new Date(date),
@@ -99,18 +44,44 @@ function displayDate(datestring, small){
 	return t[2] + "/" + t[1] + (small ? "" : "/" + t[0]);
 }
 
-function styleFeature(feat){
+function styleFeatureCasi(feat){
 	var prov = feat.properties.COD_PROV;
 	var pop = feat.properties.POP;
 	var values = data[today];
 	var value = values[prov];
 	var op = value / pop * 100 * opacityCoefficient;
 	return {
-		fillColor: '#FC4E2A',
+		fillColor: colorCasi,
 		weight: 1,
-		opacity: .5,
-		color: 'gray',
-		dashArray: '1 3',
+		opacity: op/2, //.5,
+		color: colorConfini,
+// 		dashArray: '1 3',
+		fillOpacity: op
+	};
+}
+
+function styleFeatureTrend(feat){
+	var prov = feat.properties.COD_PROV;
+	var pop = feat.properties.POP;
+	var op = 0;
+	if (today != days[0]){
+		// data di ieri
+		var y
+		for (y=0; y<days.length; y++){
+			if (days[y] == today){
+				break;
+			}
+		}
+		var values = data[today];
+		var yvalues = data[days[y-1]];
+		op = (values[prov] > 0 ? (values[prov] - yvalues[prov]) / values[prov] : 0);
+	}
+	return {
+		fillColor: colorTrend,
+		weight: 1,
+		opacity: op/2, //.5,
+		color: colorConfini,
+//		dashArray: '1 3',
 		fillOpacity: op
 	};
 }
@@ -119,8 +90,11 @@ function setDay(index){
 	theDay = days[index];
 	if (theDay) {
 		today = theDay;
-		if (geojsonLayer){
-    		geojsonLayer.setStyle(styleFeature);
+		if (geojsonLayerCasi){
+    		geojsonLayerCasi.setStyle(styleFeatureCasi);
+		}
+		if (geojsonLayerTrend){
+			geojsonLayerTrend.setStyle(styleFeatureTrend);
 		}
 		$('#lbl_last_update').html(displayDate(today));
 	}
@@ -158,8 +132,9 @@ $(function(){
             	url : "provincie.json",
             	error : function(){alert("error getting geojson")},
             	success : function(geojson){
-            		geojsonLayer = L.geoJSON(geojson, {
-            			style : styleFeature
+            		// layer dei casi
+            		geojsonLayerCasi = L.geoJSON(geojson, {
+            			style : styleFeatureCasi
             		})
             		.bindPopup(function (layer) {
 						return featureinfo(layer.feature.properties);
@@ -169,6 +144,18 @@ $(function(){
             		})
             		.addTo(theMap);
 
+//            		// layer del trend
+//            		geojsonLayerTrend = L.geoJSON(geojson, {
+//            			style : styleFeatureTrend
+//            		})
+//            		.bindPopup(function (layer) {
+//						return featureinfo(layer.feature.properties);
+//            		})
+//            		.bindTooltip(function (layer) {
+//						return layer.feature.properties.DEN_UTS;
+//            		})
+//            		;
+            		
                     L.Control.theBox({ position: 'topleft' }).addTo(theMap);
                     if (today){
                     	$('#lbl_last_update').html(displayDate(today));
@@ -199,9 +186,25 @@ $(function(){
         				}
         			});
         			
+        		    var bases = {
+    		            "Base 1" : baseLayer1,
+    		            "Base 2" : baseLayer2,
+    		        };
+    		        var overlay = {
+    		    		"Casi" : geojsonLayerCasi,
+//    		    		"Trend" : geojsonLayerTrend,
+    		        };
+
+    		        var lControl = L.control.layers(
+	        			bases, 
+	        			overlay
+		            ).addTo(theMap);
+
+        			
         			if (theMarker != undefined) {
         				theMap.removeLayer(theMarker);
-        	        };                        	}
+        	        }; 
+    	        }
             });
 			
     		days =  Object.keys(data).sort();
@@ -235,14 +238,6 @@ $(function(){
 //                 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 //                 });
     
-    var bases = {
-        "Base 1" : baseLayer1,
-        "Base 2" : baseLayer2,
-    };
-
-//                 var overlay = {
-//                 }
-// 				L.control.mousePosition().addTo(theMap);
      
     // controllo watermark
     L.Control.Watermark = L.Control.extend({
@@ -264,9 +259,5 @@ $(function(){
     };
     L.control.watermark({ position: 'topright' }).addTo(theMap);
     
-    var lControl = L.control.layers(
-        bases 
-//                    , overlay
-    ).addTo(theMap);
   
 });
